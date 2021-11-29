d9ff364565cbb7d7b65b8aed0da9c090152831a9
KeuryLendof <lendofkeury@gmail.com>
2021-11-28 22:56:17 -0400

    solicitud de vacaciones

--- src/Componentes/SolicitarVacaciones.js
+++ src/Componentes/SolicitarVacaciones.js
@@ -1,43 +1,73 @@
+import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Conexion from "./conexionVacaciones";
 import "../App.css";
+import axios from "axios";
 import logo from './img/calendar.svg'
 
+const url ="http://localhost:3001/vacaciones";
 
 
-function SolicitarVacaciones(){
+class SolicitarVacaciones extends Component{
 
+    state={
+        data:[],
+        form:{
+            id: '',
+            nombre: '',
+            fechaInicio: '',
+            fechaFin: ''
+        }
+    }
 
-    // let fechaInicio = document.getElementById("inico").value
+    peticionPost=async()=>{
+        await axios.post(url,this.state.form).then(response=>{
 
-    // console.log("Soy yoooooooooooooooo: " + fechaInicio);
+        })
+    }
 
-    return(
-        <div className="App">
-            {/* <header>
-                <h1>Calendario</h1>
-            </header> */}
+    componentDidMount(){
+    }
 
-            <div className="calendar-container">
-                <div className="calendar-info-container">
-                    <br/>
-                    <h1 className="titlev">Solicitar Vacaciones</h1>
-                    <br />
-                    <input className="input" type="text" placeholder="Estrella Adames" name="username" disabled/>
-                    <form className="inputs-container">
-                        <input id="inicio" type="date" className="input" placeholderText="Start Date"/>
-                        <input id="fin" type="date"  className="input" placeholderText="End Date"/>
-                        <button type="button" className="btnv">Solicitar</button>
-                        <Conexion fechaInicio = {document.getElementById("inico")}/>
-                        <Conexion fechaFin = "28/11/2021"/>
-                    </form>
+    handleChange=async e=>{
+        e.persist()
+        await this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        });
+        console.log(this.state.form)
+    }
+
+    render(){
+        const {form}=this.state;
+        return(
+            <div className="App">
+                {/* <header>
+                    <h1>Calendario</h1>
+                </header> */}
+    
+                <div className="calendar-container">
+                    <div className="calendar-info-container">
+                        <br/>
+                        <h1 className="titlev">Solicitar Vacaciones</h1>
+                        <br />
+                        <input className="input" type="text" placeholder="Estrella Adames" name="nombre" id="nombre" onChange={this.handleChange} value={form.nombre}/>
+                        <form className="inputs-container">
+                            <input name="fechaInicio" id="fechaInicio" type="date" className="input" placeholderText="Start Date" onChange={this.handleChange} value={form.fechaInicio}/>
+                            <input name="fechaFin" id="fechaFin" type="date"  className="input" placeholderText="End Date" onChange={this.handleChange} value={form.fechaFin}/>
+                            <button type="button" className="btnv" onClick={()=>this.peticionPost()}>Solicitar</button>
+                        </form>
+                    </div>
+                    <img className="calendar-image-container" src={logo}/>
                 </div>
-                <img className="calendar-image-container" src={logo}/>
             </div>
-        </div>
-    )
+        )
+    }
+
+
     
 }
 
--- src/Componentes/Vacaciones.js
+++ src/Componentes/Vacaciones.js
@@ -1,50 +1,88 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 import '../App.css';
-import prueba from "..";
 
-function Vacaciones(){
+const url ="http://localhost:3001/vacaciones";
 
-    return(
-        <div className="revisarVacaciones">
-            <section class="body-chat">
-                <div class="seccion-titulo">
-                    <h3>
-                        <i class="fas fa-comments"></i>
-                        Solicitudes de vacaciones
-                    </h3>
-                    <button onClick={prueba}>Ver Solicitudes</button>
-                </div>
-                <div class="panel-chat">
-                    <div class="mensaje">
-                        <div class="avatar">
-                            <img src="ruta_img" alt="img"/>
-                        </div>
-                        <div class="cuerpo">
-                            <div id="texto" class="texto">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing, elit. Dolor eligendi voluptatum dolore voluptas iure.
-                                <span class="tiempo">
-                                    <i class="far fa-clock"></i>
-                                    Hace 5 min
-                                </span>
+
+class Vacaciones extends Component{
+
+    state={
+        data:[]
+    }
+
+    peticionGet=()=>{
+        axios.get(url).then(response=>{
+            this.setState({data: response.data});
+        })
+    }
+
+    componentDidMount(){
+        this.peticionGet();
+    }
+
+    render(){
+        return(
+            <div className="revisarVacaciones">
+                <section class="body-chat">
+                    <div class="seccion-titulo">
+                        <h3>
+                            <i class="fas fa-comments"></i>
+                            Solicitudes de vacaciones
+                        </h3>
+                    </div>
+                    <div class="panel-chat">
+                        <div class="mensaje">
+                            <div class="avatar">
+                                {/* <img src="ruta_img" alt="img"/> */}
                             </div>
-                            <ul class="opciones-msj">
-                                <li>
-                                    <button type="button">
-                                        <i class="fas fa-times"></i>
-                                    </button>
-                                </li>
-                                <li>
-                                    <button type="button">
-                                        <i class="fas fa-share-square"></i>
-                                    </button>
-                                </li>
-                            </ul>
+                            <div class="cuerpo">
+                                <div class="texto">
+                                    <table>
+                                        <thead>
+                                            <th>Nombre</th>
+                                            <th>Inicio</th>
+                                            <th>Fin</th>
+                                            <th>Select</th>
+                                        </thead>
+                                        <tbody>
+                                            {this.state.data.map(vacaciones=>{
+                                                return(
+                                                    <tr>
+                                                        <td>{vacaciones.nombre}</td>
+                                                        <td>{vacaciones.fechaInicio}</td>
+                                                        <td>{vacaciones.fechaFin}</td>
+                                                        <td><input type="radio"/></td>
+                                                    </tr>  
+                                                )
+                                            })}
+                                        </tbody>
+                                    </table>
+                                    {/* <span class="tiempo">
+                                        <i class="far fa-clock"></i>
+                                        Hace 5 min
+                                    </span> */}
+                                </div>
+                                {/* <ul class="opciones-msj">
+                                    <li>
+                                        <button type="button">
+                                            <i class="fas fa-times"></i>
+                                        </button>
+                                    </li>
+                                    <li>
+                                        <button type="button">
+                                            <i class="fas fa-share-square"></i>
+                                        </button>
+                                    </li>
+                                </ul> */}
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div>
-    )
+                </section>
+            </div>
+        )
+    }
+
 }
 
 export default Vacaciones;
